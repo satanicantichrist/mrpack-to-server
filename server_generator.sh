65eee016a94cc267531b20573cb806a217590bf8
satanicantichrist <burianek.jan@protonmail.com>
2024-07-13 14:40:44 +0200

    Added support for neoforge modpacks

--- server_generator.sh
+++ server_generator.sh
@@ -13,6 +13,7 @@ data=$(cat "$tmpdir/modrinth.index.json")
 packversion=$(echo $data | jq '."versionId"'  | cut -d "\"" -f2)
 totalmods=$(echo $data | jq '."files" | length')
 fabric=$(echo $data | jq '."dependencies"."fabric-loader"'  | cut -d "\"" -f2)
+neoforge=$(echo $data | jq '."dependencies"."neoforge"'  | cut -d "\"" -f2)
 minecraft=$(echo $data | jq '."dependencies"."minecraft"'  | cut -d "\"" -f2)
 path=server
 optionalall=0
@@ -21,9 +22,6 @@ mkdir -p "$path/mods"
 
 echo Copying overrides
 cp -a $tmpdir/overrides/* $path
-
-echo Copying server start script
-cp default_server_start_script.sh $path/start.sh
 
 for ((i = 0; i < $totalmods; i++)) do
 
@@ -76,11 +74,22 @@ for ((i = 0; i < $totalmods; i++)) do
 	fi
 done
 
-echo Downloading fabric server file
-curl -A "$useragent" --progress-bar -s https://meta.fabricmc.net/v2/versions/loader/$minecraft/$fabric/1.0.1/server/jar -o $path/server.jar
-echo "fabric-loader-version: $fabric; minecraft-version: $minecraft; pack-version: $packversion" > $path/versions.txt
+if [[ "$fabric" != "null" ]]; then
+	echo Downloading fabric server file
+	curl -A "$useragent" --progress-bar -s https://meta.fabricmc.net/v2/versions/loader/$minecraft/$fabric/1.0.1/server/jar -o $path/server.jar
+	echo "fabric-loader-version: $fabric; minecraft-version: $minecraft; pack-version: $packversion" > $path/versions.txt
+	echo Copying server start script for fabric
+	cp default_server_start_script.sh $path/start.sh
+	echo Server generated. To start server, run start.sh
+fi
+
+if [[ "$neoforge" != "null" ]]; then
+        echo Downloading neoforge installer
+        curl -A "$useragent" --progress-bar -s https://maven.neoforged.net/releases/net/neoforged/neoforge/$neoforge/neoforge-$neoforge-installer.jar -o $tmpdir/neoforge-installer.jar
+	echo Installing neoforge server
+	java -jar $tmpdir/neoforge-installer.jar --install-server ./server
+        echo "neoforge-version: $neoforge; minecraft-version: $minecraft; pack-version: $packversion" > $path/versions.txt
+	echo Server generated. To start server, run run.sh
+fi
 
 rm -rf "$tmpdir"
-
-
-echo Server generated. To start server, run start.sh
